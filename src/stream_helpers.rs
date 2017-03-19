32c6bf69a460915065d60995ede3b86c98dc83d6
Robey Pointer <robeypointer@gmail.com>
2017-03-18 22:54:05 -0700

    flatten_stream

--- src/stream_helpers.rs
+++ src/stream_helpers.rs
@@ -24,14 +24,14 @@ pub fn make_stream_4(b1: Bytes, b2: Bytes, b3: Bytes, b4: Bytes) -> impl Stream<
   stream::iter(vec![ Ok(vec![ b1 ]), Ok(vec![ b2 ]), Ok(vec![ b3 ]), Ok(vec![ b4 ]) ])
 }
 
-// convert a stream into a vector of hex output, for tests
+// convert a stream into a vector of hex output (for tests)
 pub fn hex_stream<T>(s: T) -> Vec<String>
   where T: Stream<Item = Vec<Bytes>, Error = io::Error>
 {
   s.collect().wait().unwrap().iter().map(|b| b.to_hex()).collect::<Vec<String>>()
 }
 
-// convert a stream into a vector of string output, for tests
+// convert a stream into a vector of string output (for tests)
 pub fn string_stream<T>(s: T) -> Vec<String>
   where T: Stream<Item = Vec<Bytes>, Error = io::Error>
 {
@@ -40,9 +40,9 @@ pub fn string_stream<T>(s: T) -> Vec<String>
   }).collect::<Vec<String>>()
 }
 
-// convert a stream into a single Bytes
-pub fn drain_stream<T>(s: T) -> Vec<u8>
-  where T: Stream<Item = Vec<Bytes>, Error = io::Error>
+// convert a stream into a single buffer (for tests)
+pub fn drain_stream<S>(s: S) -> Vec<u8>
+  where S: Stream<Item = Vec<Bytes>, Error = io::Error>
 {
   let mut rv: Vec<u8> = Vec::new();
   for vec in s.collect().wait().unwrap() {
@@ -51,4 +51,11 @@ pub fn drain_stream<T>(s: T) -> Vec<u8>
     }
   }
   rv
+}
+
+// convert a stream of `Vec<Bytes>` into a stream of `Bytes`
+pub fn flatten_stream<S>(s: S) -> impl Stream<Item = Bytes, Error = io::Error>
+  where S: Stream<Item = Vec<Bytes>, Error = io::Error>
+{
+  s.map(|vec| stream::iter(vec.into_iter().map(|b| Ok(b)))).flatten()
 }
