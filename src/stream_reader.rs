926ffa1a8bd77dfc07a6ce447dc62d7b5a10139f
Robey Pointer <robeypointer@gmail.com>
2017-03-20 00:15:02 -0700

    struggled in the net for a long time before getting here, but it works.

--- src/stream_reader.rs
+++ src/stream_reader.rs
@@ -5,52 +5,109 @@ use std::collections::VecDeque;
 use std::io;
 
 /*
- * wrapper for Stream where you may call `read` to get specific-sized buffers
- * out.
- *
- * FIXME: i bet buffer_stream could be implemented using this.
+ * Read exactly `count` bytes from a stream of `Bytes` objects, returning a
+ * `Vec<Bytes>` containing the cumulative buffers totalling exactly the
+ * desired bytes, and a new `Stream` representing everything afterwards.
+ * If not enough bytes are available on the stream before EOF, an EOF error
+ * is returned.
  */
-pub fn stream_reader<S>(s: S) -> StreamReader<S> where S: Stream<Item = Bytes, Error = io::Error> {
-  StreamReader::new(s)
+pub fn stream_read_exact<S>(s: S, count: usize)
+  -> impl Future<Item = (Vec<Bytes>, impl Stream<Item = Bytes, Error = io::Error>), Error = io::Error>
+  where S: Stream<Item = Bytes, Error = io::Error>
+{
+  read_from_stream(s, count, true, true)
 }
 
-pub struct StreamReader<S> where S: Stream<Item = Bytes, Error = io::Error> {
-  stream: Fuse<S>,
+/*
+ * Read no more than `count` bytes from a stream of `Bytes` objects, returning
+ * a `Vec<Bytes>` containing the cumulative buffers totalling up to (but no
+ * more than) the desired bytes, and a new `Stream` representing everything
+ * afterwards. If fewer bytes are returned, the stream hit EOF.
+ */
+pub fn stream_read<S>(s: S, count: usize)
+  -> impl Future<Item = (Vec<Bytes>, impl Stream<Item = Bytes, Error = io::Error>), Error = io::Error>
+  where S: Stream<Item = Bytes, Error = io::Error>
+{
+  read_from_stream(s, count, true, false)
+}
+
+/*
+ * Read at least `count` bytes from a stream of `Bytes` objects, if possible,
+ * returning a `Vec<Bytes>` containing the cumulative buffers, and a new
+ * `Stream` representing everything afterwards. If fewer bytes are returned,
+ * the stream hit EOF. More bytes may be returned if it would avoid splitting
+ * a `Bytes` object.
+ */
+pub fn stream_read_buffered<S>(s: S, block_size: usize)
+  -> impl Future<Item = (Vec<Bytes>, impl Stream<Item = Bytes, Error = io::Error>), Error = io::Error>
+  where S: Stream<Item = Bytes, Error = io::Error>
+{
+  read_from_stream(s, block_size, false, false)
+}
+
+// (stream, count, at_most, at_least) -> (vec, stream)
+// any remainder has been prefixed back into the returned stream.
+pub fn read_from_stream<S>(s: S, count: usize, at_most: bool, at_least: bool)
+  -> impl Future<Item = (Vec<Bytes>, impl Stream<Item = Bytes, Error = io::Error>), Error = io::Error>
+  where S: Stream<Item = Bytes, Error = io::Error>
+{
+  read_from_stream_raw(s, count, at_most, at_least).map(|(buffer, remainder, stream)| {
+    let merged = stream::iter(remainder.into_iter().map(|b| Ok(b))).chain(stream);
+    ( buffer, merged )
+  })
+}
+
+// lowest-level call. perform a read, then return the read, any remainder, and the original stream.
+pub fn read_from_stream_raw<S>(s: S, count: usize, at_most: bool, at_least: bool)
+  -> impl Future<Item = (Vec<Bytes>, Option<Bytes>, S), Error = io::Error>
+  where S: Stream<Item = Bytes, Error = io::Error>
+{
+  StreamReader {
+    stream: Some(s.fuse()),
+    count: count,
+    at_most: at_most,
+    at_least: at_least,
+    saved: VecDeque::new(),
+    total_saved: 0,
+    error: None
+  }
+}
+
+#[must_use = "futures do nothing unless polled"]
+struct StreamReader<S> where S: Stream<Item = Bytes, Error = io::Error> {
+  stream: Option<Fuse<S>>,
+  count: usize,
+  at_most: bool,
+  at_least: bool,
+
+  // internal state:
   saved: VecDeque<Bytes>,
   total_saved: usize,
   error: Option<io::Error>
 }
 
 impl<S> StreamReader<S> where S: Stream<Item = Bytes, Error = io::Error> {
-  pub fn new(s: S) -> StreamReader<S> {
-    StreamReader {
-      stream: s.fuse(),
-      saved: VecDeque::new(),
-      total_saved: 0,
-      error: None
-    }
-  }
-
   /*
-   * "read" up to `count` bytes by shuffling `Bytes` objects (without
-   * copying buffers).
+   * "read" up to `count` bytes by shuffling `Bytes` objects, returning a
+   * vector to avoid buffer-copying. you can flatten the result at your
+   * leisure if that's what you want.
    *   - if `at_most` is true, a `Bytes` may be split to return exactly
    *     `count` bytes.
    *   - if there aren't `count` bytes buffered, you'll get less than you
    *     asked for. to prevent this, check `total_saved` before calling.
    */
-  fn drain(&mut self, count: usize, at_most: bool) -> Vec<Bytes> {
+  fn drain(&mut self) -> Vec<Bytes> {
     let mut rv: Vec<Bytes> = Vec::new();
     let mut so_far = 0;
 
-    while self.saved.len() > 0 && so_far < count {
+    while self.saved.len() > 0 && so_far < self.count {
       let chunk = self.saved.pop_front().unwrap();
-      if (so_far + chunk.len() <= count) || !at_most {
+      if (so_far + chunk.len() <= self.count) || !self.at_most {
         so_far += chunk.len();
         self.total_saved -= chunk.len();
         rv.push(chunk);
       } else {
-        let n = count - so_far;
+        let n = self.count - so_far;
         so_far += n;
         self.total_saved -= n;
         rv.push(chunk.slice(0, n));
@@ -61,74 +118,65 @@ impl<S> StreamReader<S> where S: Stream<Item = Bytes, Error = io::Error> {
     rv
   }
 
-  pub fn read_exact<'a>(&'a mut self, count: usize) -> impl 'a + Future<Item = Vec<Bytes>, Error = io::Error> {
-    StreamReaderFuture::<'a> { reader: self, count: count, at_most: true, at_least: true }
-  }
-
-  pub fn read<'a>(&'a mut self, count: usize) -> impl 'a + Future<Item = Vec<Bytes>, Error = io::Error> {
-    StreamReaderFuture::<'a> { reader: self, count: count, at_most: true, at_least: false }
-  }
-
-  // convert any un-read data back into a stream.
-  pub fn into_stream(self) -> impl Stream<Item = Bytes, Error = io::Error> {
-    stream::iter(self.saved.into_iter().map(|b| Ok(b))).chain(self.stream)
+  /*
+   * assuming we have collected as many bytes as we need, or as many bytes
+   * as we CAN, drain off enough `Bytes` objects to fill the original request
+   * (or try), return any unused buffer (there should be at most one), and
+   * return the original stream.
+   */
+  fn complete(&mut self) -> (Vec<Bytes>, Option<Bytes>, S) {
+    let buffer = self.drain();
+    assert!(self.saved.len() <= 1);
+    let remainder = self.saved.pop_front();
+    let stream = self.stream.take().unwrap().into_inner();
+    (buffer, remainder, stream)
   }
 }
 
-
-// ----- StreamReaderFuture
-
-pub struct StreamReaderFuture<'a, S> where S: 'a + Stream<Item = Bytes, Error = io::Error> {
-  reader: &'a mut StreamReader<S>,
-  count: usize,
-  at_most: bool,    // do not return more than `count` bytes.
-  at_least: bool    // do not return less than `count` bytes (error on EOF).
-}
-
-impl<'a, S> Future for StreamReaderFuture<'a, S> where S: Stream<Item = Bytes, Error = io::Error> {
-  type Item = Vec<Bytes>;
+impl<S> Future for StreamReader<S> where S: Stream<Item = Bytes, Error = io::Error> {
+  type Item = (Vec<Bytes>, Option<Bytes>, S);
   type Error = io::Error;
 
   fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
     loop {
-      if self.reader.total_saved >= self.count {
-        return Ok(Async::Ready(self.reader.drain(self.count, self.at_most)))
+      if self.total_saved >= self.count {
+        return Ok(Async::Ready(self.complete()))
       }
 
-      if let Some(error) = self.reader.error.take() {
+      if let Some(error) = self.error.take() {
         // if there's no minimum len, drain the remainder before reporting the error.
-        if self.reader.saved.len() > 0 && !self.at_least {
-          return Ok(Async::Ready(self.reader.drain(self.count, self.at_most)));
+        if self.saved.len() > 0 && !self.at_least {
+          return Ok(Async::Ready(self.complete()));
         } else {
           return Err(error);
         }
       }
 
-      match self.reader.stream.poll() {
+      match self.stream.as_mut().expect("polling stream twice").poll() {
         Ok(Async::NotReady) => {
           return Ok(Async::NotReady);
         }
 
         // end of stream
         Ok(Async::Ready(None)) => {
-          if (self.at_least && (self.reader.total_saved < self.count)) || self.reader.total_saved == 0 {
+          if self.at_least && (self.total_saved < self.count) {
             return Err(io::Error::new(io::ErrorKind::UnexpectedEof, "EOF"));
           } else {
-            return Ok(Async::Ready(self.reader.drain(self.count, self.at_most)));
+            return Ok(Async::Ready(self.complete()));
           }
         }
 
         Ok(Async::Ready(Some(buffer))) => {
-          self.reader.total_saved += buffer.len();
-          self.reader.saved.push_back(buffer);
+          self.total_saved += buffer.len();
+          self.saved.push_back(buffer);
           // fall through to check if we have enough buffered to exit.
         }
 
-        // in rust streams, errors float downsteam as if they were items
+        // in rust streams, errors float downsteam as if they were items.
         // it's really fucking weird. mimic the streams library by allowing
         // any buffered data to be processed before puking the error.
         Err(error) => {
-          self.reader.error = Some(error);
+          self.error = Some(error);
           // fall through to drain all the pre-error buffers.
         }
       }
--- tests/test_stream_reader.rs
+++ tests/test_stream_reader.rs
@@ -7,43 +7,90 @@ mod tests {
   use bytes::{Bytes};
   use futures::{Future, Stream};
   use lib4bottle::{ToHex};
-  use lib4bottle::stream_helpers::{flatten_stream, make_stream_1};
-  use lib4bottle::stream_reader::{stream_reader};
+  use lib4bottle::stream_helpers::{flatten_stream, make_stream, make_stream_1};
+  use lib4bottle::stream_reader::{stream_read, stream_read_buffered, stream_read_exact};
 
   #[test]
-  fn read_exact_slices() {
+  fn stream_read_exact_slices() {
     let s = flatten_stream(make_stream_1(Bytes::from_static(b"progressive")));
-    let mut r = stream_reader(s);
-    assert_eq!(r.read_exact(3).wait().unwrap().to_hex(), "70726f");
-    assert_eq!(r.read_exact(2).wait().unwrap().to_hex(), "6772");
-    assert_eq!(r.read_exact(5).wait().unwrap().to_hex(), "6573736976");
-    assert_eq!(r.read_exact(1).wait().unwrap().to_hex(), "65");
-    assert!(r.read_exact(1).wait().is_err());
+    let (data1, s1) = stream_read_exact(s, 3).wait().unwrap();
+    assert_eq!(data1.to_hex(), "70726f");
+    let (data2, s2) = stream_read_exact(s1, 2).wait().unwrap();
+    assert_eq!(data2.to_hex(), "6772");
+    let (data3, s3) = stream_read_exact(s2, 5).wait().unwrap();
+    assert_eq!(data3.to_hex(), "6573736976");
+    let (data4, s4) = stream_read_exact(s3, 1).wait().unwrap();
+    assert_eq!(data4.to_hex(), "65");
+    assert!(stream_read_exact(s4, 1).wait().is_err());
   }
 
   #[test]
-  fn read_exact_refuses_to_truncate() {
-    let s = flatten_stream(make_stream_1(Bytes::from_static(b"progressive")));
-    let mut r = stream_reader(s);
-    assert!(r.read_exact(12).wait().is_err());
+  fn stream_read_exact_slices_from_chunks() {
+    let s = flatten_stream(make_stream(vec![
+      Bytes::from_static(b"pr"),
+      Bytes::from_static(b"ogres"),
+      Bytes::from_static(b"s"),
+      Bytes::from_static(b"i"),
+      Bytes::from_static(b"ve")
+    ]));
+    let (data1, s1) = stream_read_exact(s, 3).wait().unwrap();
+    assert_eq!(data1.to_hex(), "70726f");
+    let (data2, s2) = stream_read_exact(s1, 2).wait().unwrap();
+    assert_eq!(data2.to_hex(), "6772");
+    let (data3, s3) = stream_read_exact(s2, 5).wait().unwrap();
+    assert_eq!(data3.to_hex(), "6573736976");
+    let (data4, s4) = stream_read_exact(s3, 1).wait().unwrap();
+    assert_eq!(data4.to_hex(), "65");
+    assert!(stream_read_exact(s4, 1).wait().is_err());
   }
 
   #[test]
-  fn drain_after_read() {
+  fn stream_read_exact_refuses_to_truncate() {
     let s = flatten_stream(make_stream_1(Bytes::from_static(b"progressive")));
-    let mut r = stream_reader(s);
-    assert_eq!(r.read_exact(3).wait().unwrap().to_hex(), "70726f");
-    assert_eq!(r.into_stream().collect().wait().unwrap().to_hex(), "6772657373697665");
+    assert!(stream_read_exact(s, 12).wait().is_err());
   }
 
   #[test]
-  fn read() {
+  fn stream_read_exact_returns_valid_continuation_stream() {
     let s = flatten_stream(make_stream_1(Bytes::from_static(b"progressive")));
-    let mut r = stream_reader(s);
-    assert_eq!(r.read(3).wait().unwrap().to_hex(), "70726f");
-    assert_eq!(r.read(2).wait().unwrap().to_hex(), "6772");
-    assert_eq!(r.read(5).wait().unwrap().to_hex(), "6573736976");
-    assert_eq!(r.read(2).wait().unwrap().to_hex(), "65");
-    assert!(r.read(1).wait().is_err());
+    let (data1, s1) = stream_read_exact(s, 3).wait().unwrap();
+    assert_eq!(data1.to_hex(), "70726f");
+    assert_eq!(s1.collect().wait().unwrap().to_hex(), "6772657373697665");
+  }
+
+  #[test]
+  fn stream_read_works() {
+    let s = flatten_stream(make_stream_1(Bytes::from_static(b"progressive")));
+    let (data1, s1) = stream_read(s, 3).wait().unwrap();
+    assert_eq!(data1.to_hex(), "70726f");
+    let (data2, s2) = stream_read(s1, 2).wait().unwrap();
+    assert_eq!(data2.to_hex(), "6772");
+    let (data3, s3) = stream_read(s2, 5).wait().unwrap();
+    assert_eq!(data3.to_hex(), "6573736976");
+    let (data4, s4) = stream_read(s3, 2).wait().unwrap();
+    assert_eq!(data4.to_hex(), "65");
+    let (data5, s5) = stream_read(s4, 1).wait().unwrap();
+    assert_eq!(data5.to_hex(), "");
+    assert_eq!(s5.collect().wait().unwrap().to_hex(), "");
+  }
+
+  #[test]
+  fn stream_read_buffered_works() {
+    let s = flatten_stream(make_stream(vec![
+      Bytes::from_static(b"pr"),
+      Bytes::from_static(b"ogres"),
+      Bytes::from_static(b"siv"),
+      Bytes::from_static(b"e tran"),
+      Bytes::from_static(b"ce")
+    ]));
+    let (data1, s1) = stream_read_buffered(s, 3).wait().unwrap();
+    assert_eq!(data1.to_hex(), "70726f67726573");
+    let (data2, s2) = stream_read_buffered(s1, 2).wait().unwrap();
+    assert_eq!(data2.to_hex(), "736976");
+    let (data3, s3) = stream_read_buffered(s2, 10).wait().unwrap();
+    assert_eq!(data3.to_hex(), "65207472616e6365");
+    let (data4, s4) = stream_read_buffered(s3, 10).wait().unwrap();
+    assert_eq!(data4.to_hex(), "");
+    assert_eq!(s4.collect().wait().unwrap().to_hex(), "");
   }
 }
